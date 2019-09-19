e34f1a9c64cd113524ddad0ce30d7876a0d45ccc
Yan Shevtsov <fatwebdev@gmail.com>
2019-09-19 16:22:28 +0300

    Lesson 4

---
+++ Lesson_04/station.rb
@@ -0,0 +1,38 @@
+class Station
+  attr_reader :trains, :name
+
+  def initialize(name)
+    @name = name
+
+    @trains = {}
+  end
+
+  def take_train(train)
+    if trains[train.type]
+      trains[train.type] << train
+    else
+      trains[train.type] = [train]
+    end
+  end
+
+  def send_train(train)
+    train.forward
+  end
+
+  def delete_train(train)
+    trains[train.type].delete(train)
+  end
+
+  def show_trains
+    if trains.empty?
+      puts 'There are no trains at the station.'
+      return
+    end
+
+    puts 'There are trains at the station by type:'
+    trains.each do |type, trains_this_type|
+      puts "\t#{type}:"
+      trains_this_type.each { |train| puts "\t\t#{train.number}" }
+    end
+  end
+end
